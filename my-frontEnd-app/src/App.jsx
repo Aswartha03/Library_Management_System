0f2c45970023c572237dba636df3fea42c78fc8e
Aswarth <212g1a0403@gmail.com>
2025-09-23 10:28:03 +0530

    commit

--- my-frontEnd-app/src/App.jsx
+++ my-frontEnd-app/src/App.jsx
@@ -7,11 +7,12 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Profile from "./components/profile";
 import EditProfile from "./components/EditProfile";
-import ForgotPassword from "./components/forgotPassword";
+
 import ResetPassword from "./components/ResetPassword";
 import LibrarianDashboard from "./components/Books/LibrarianDashboard";
 import AddBook from "./pages/addBook";
 import UpdateBook from "./components/Books/UpdateBook";
+import ForgotPassword from './components/ForgotPassword';
 function App() {
   return (
     <>
@@ -23,7 +24,7 @@ function App() {
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/librarian-dashboard" element={<LibrarianDashboard />} />
         <Route path="/add-book" element={<AddBook />} />
